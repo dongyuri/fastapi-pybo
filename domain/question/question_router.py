957e792c3796f6629baa6b6c280729b127a5404f
김동률(DongRyul Kim) <drkim@surromind.ai>
2023-02-14 14:23:11 +0900

    2-04-2 의존성 주입

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,7 +1,8 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from database import SessionLocal
+from database import get_db
 from models import Question
 
 router = APIRouter(
@@ -10,9 +11,6 @@ router = APIRouter(
 
 
 @router.get("/list")
-def question_list():
-    db = SessionLocal()
+def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    db.close()
     return _question_list
-
