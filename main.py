87be0f0d42c086b548f2209fc78ed1be425795c8
drkim <dongyuri@gmail.com>
2025-09-25 17:44:45 +0900

    3-07 회원가입

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from domain.question import question_router
 from domain.answer import answer_router
+from domain.user import user_router
 
 
 app = FastAPI()
@@ -28,4 +30,5 @@ def hello():
 
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
+app.include_router(user_router.router)
 
